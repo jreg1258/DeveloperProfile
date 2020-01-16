a74f28703289014513ec2c49e98fd8fd09342495
Joey Register <josephregister1258@gmail.com>
2020-01-16 17:57:04 -0500

    pulled user, image, and profile to variables

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ inquirer
 
   .then((res) => {
     const profileInfo = new Array(res.data.items[0])
-    
+    console.log(profileInfo)
+
 const user = profileInfo.map(({ login }) => login)
+const image = profileInfo.map(({ avatar_url }) => avatar_url)
+const profile= profileInfo.map(({ html_url }) => html_url)
+const user = profileInfo.map(({ login }) => login)
+
 console.log(user)
     })
 .catch((err) => console.error(err))
