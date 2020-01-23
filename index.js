0adff780ee3696829c80c875ffdc3ffbd32e4bbd
Joey Register <josephregister1258@gmail.com>
2020-01-23 18:14:49 -0500

    final edits, added css to html for use with pdf

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ inquirer
   .then(function ({ username, color }) {
     const queryUrl = `https://api.github.com/users/${username}`
 
-    fs.writeFileSync("index.html", `<!DOCTYPE html>
-    <html lang="en">
+    fs.writeFileSync("index.html", 
+    `<!DOCTYPE html>
+    <html lang="en" style="background-color: ${ color }">
     <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
@@ -28,9 +29,34 @@ inquirer
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>${username}'s Developer Profile</title>
+    <style>
+    #follow {
+    padding-top: 20px;
+    }
+    
+img { 
+        margin: auto;
+        display: block;
+    }
+
+a {
+    display: block;
+    margin: auto;
+}
+
+.jumbotron {
+    text-align: center;
+    width: 800px;
+    margin: auto;
+}
+
+body,html,.jumbotron {
+    height: auto;
+}
+</style>
     </head>
     <body style="background-color: ${ color }">
-    <div class="jumbotron" style="background-color: ${ color }">`)
+    <div class="jumbotron jumbotron-fluid" style="background-color: ${ color }">`)
 
     return axios.get(queryUrl)
   })
@@ -50,46 +76,54 @@ inquirer
     
     starredURL = starredURL.substring(0, starredURL.length-15)
     fs.appendFileSync("index.html", 
-    `\n<h1>${user}'s Developer Profile</h1>
-    <img src="${image}" class="rounded-pill" alt="Profile Pic">
-    
-       
-        
-                
-    <h2 class="text-wrap" id="follow" style="font-family: 'Lobster', cursive;>${bio}</h2>
-              <h4>Followers: ${followers} || Following: ${following}</h4>
-          
-          <h3># of Repositories: ${repos}</h4>
-       
-        
-      
-          
-       
-<hr class="my-4">
-<hr class="my-4">
-<hr class="my-4">
-                
-                <a href="${ blog}"><h4>Blog<h4></a>
-              <br>
-                    <a href="${userLocation}"><h4>${userLocation}</h4></a>
-                <br>
-                  <a href="${profile}"><h4>${profile}</h4></a>`)
+`\n<h1>${user}'s Developer Profile</h1>
+<img src="${image}" class="rounded-pill" alt="Profile Pic">
+<br>
+<br>         
+<h2 class="text-wrap" id="follow" style="font-family: 'Lobster', cursive;">${bio}</h2>
+<h4>Followers: ${followers} || Following: ${following}</h4>
+<h3># of Repositories: ${repos}</h3>   
+<br>
+<br>
+<br>
+</div>
+<footer> <div class="container-fluid">
+<div class="row">
+<div class="col-sm">
+<a href="${ blog}"><h4 style="text-align: center;">Blog<h4></a>
+</div>
+<div class="col-sm">
+<a href="${profile}"><h4 style="text-align: center;">GitHub Profile</h4></a>
+</div>
+<div class="col-sm">
+<a href="${userLocation}"><h4 style="text-align: center;">Current City</h4></a>
+</div>`)
 
 return axios.get(starredURL)
 })
 .then((res) => {
   const starred = res.data.length 
-  fs.appendFileSync("index.html", `\n<br>
-  <h3>Starred Repos: ${ starred }</h3>  
-  </div>   
+  fs.appendFileSync("index.html", 
+  `\n<br>
+  </div>
+  <div class="row">
+  <div class="col-sm">
+  <h3 style="text-align: center;">Starred Repos: ${ starred }</h3>  
+  </div>
+  </div>
+  </div>
+  </footer>   
   </body>
   </html>`)
     const html = fs.readFileSync("index.html", "utf8")
-    pdf.create(html, options).toFile("profile.pdf", (err) => {
+    const donePDF = pdf.create(html, options).toFile("./profile.pdf", (err) => {
       if (err) {
         console.log(err)
       }
       console.log("PDF created!")
     })
+    return fs.writeFileSync("./profile.pdf", donePDF)
 })
   .catch((err) => console.error(err))
+
+  
